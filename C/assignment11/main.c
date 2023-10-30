103591ec51d6e0f5cf8c3d362c71e19a4992e245
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-10-30 07:35:29 +0200

    feat: :sparkles: assignment 11 : Decimal to Binary without recursive function

---
+++ C/assignment11/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Task 11 : Decimal to Binary without recursive function
+
+void main (){
+
+    int value;
+
+    printf("Enter a Decimal value: ");
+    scanf("%d", &value);
+
+
+        int binary[10];
+        int i = 0;
+
+        while (value > 0) {
+            binary[i] = value % 2;
+            value = value / 2;
+            i++;
+        }
+
+        printf("Binary: ");
+        for (int j = i - 1; j >= 0; j--) {
+            printf("%d", binary[j]);
+        }
+
+}
