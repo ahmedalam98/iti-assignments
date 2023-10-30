bed690639cf09017e5005a67a37424b5465c16b6
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-10-30 07:37:03 +0200

    feat: :sparkles: assignment 13 : Factorial Number using For Loop

---
+++ C/assignment13/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Task 13 : Factorial Number using For Loop
+
+int factorial(int n) {
+    int result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    return result;
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number < 0) {
+        printf("Enter a positive number next time.\n");
+    } else {
+        int result = factorial(number);
+        printf("For Loop Factorial of %d is : %d\n", number, result);
+    }
+
+    return 0;
+}
