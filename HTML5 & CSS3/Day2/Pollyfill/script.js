dc2840d367c0b02d15435c32065045f1cb3268db
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2024-01-03 17:28:52 +0200

    feat: :sparkles: Add HTML5 Day2

---
+++ HTML5 & CSS3/Day2/Pollyfill/script.js
@@ -0,0 +1,47 @@
+if (window.localStorage) {
+  let myLocalStorage = {
+    cookiesObj: {},
+
+    setItem: function (key, val) {
+      document.cookie = key + "=" + val;
+      this.cookiesObj[key] = val;
+    },
+
+    getItem: function (key) {
+      return this.cookiesObj[key];
+    },
+
+    removeItem: function (key) {
+      var date = new Date();
+      date.setFullYear(date.getFullYear() - 1);
+      document.cookie = key + "=" + ";expires=" + date.toUTCString();
+    },
+
+    clear: function () {
+      var date = new Date();
+      date.setFullYear(date.getFullYear() + 1);
+      this.separate();
+      for (const key in this.cookiesObj) {
+        document.cookie = key + "=" + ";expires=" + date.toUTCString();
+      }
+    },
+
+    separate: function () {
+      var keyVal = document.cookie.split(/=|;/);
+
+      for (var i = 0; i < keyVal.length; i++) {
+        var key = keyVal[i].trim();
+        var val = keyVal[i + 1];
+        i++;
+        this.cookiesObj[key] = val;
+      }
+    },
+  };
+
+  window.myLocalStorage = myLocalStorage;
+
+  myLocalStorage.clear();
+  myLocalStorage.setItem("userName", "Ahmed Alam El-Deen");
+  myLocalStorage.setItem("age", "25");
+  myLocalStorage.getItem("userName");
+}
