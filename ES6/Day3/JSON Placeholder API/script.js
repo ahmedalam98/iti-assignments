33064305e839957a04c3fa16626586c7323d7e5f
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2024-01-05 17:26:58 +0200

    style: :art: Add button to fetch data

--- ES6/Day3/JSON Placeholder API/script.js
+++ ES6/Day3/JSON Placeholder API/script.js
@@ -1,32 +1,32 @@
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
+  const myBtn = document.getElementById("btn");
+
+  myBtn.addEventListener("click", async function () {
+    try {
+      let data = await getPosts();
+
+      let output = "";
+      data.forEach((post) => {
+        output += `
+          <tr>
+            <td>${post.id}</td>
+            <td>${post.title}</td>
+          </tr>
+        `;
+      });
+
+      document.getElementById("outputBody").innerHTML = output;
+      document.getElementById("errorCase").innerHTML = "";
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      let output = `Failed to fetch data ❌`;
+      document.getElementById("errorCase").innerHTML = output;
+    }
+  });
+
   async function getPosts() {
     let response = await fetch("https://jsonplaceholder.typicode.com/posts");
     let data = await response.json();
-    // console.log(data);
     return data;
-  }
-
-  try {
-    let data = await getPosts();
-
-    let output = "";
-    data.forEach((post) => {
-      output += `
-        <tr>
-          <td>${post.id}</td>
-          <td>${post.title}</td>
-        </tr>
-      `;
-    });
-
-    document.getElementById("outputBody").innerHTML = output;
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    let output = `
-      <div style="text-align: center; font-size: 28px;">
-        <p>Failed to fetch data ❌</p>
-      </div>
-    `;
-    document.getElementById("errorCase").innerHTML = output;
   }
 });
