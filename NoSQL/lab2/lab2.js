88ed73e8504d242e6941366685fa1b187144396c
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2024-01-17 16:53:08 +0200

    refactor: :bulb: Add comments

--- NoSQL/lab2/lab2.js
+++ NoSQL/lab2/lab2.js
@@ -1,13 +1,13 @@
-// a
+// a ) Display all documents in instructors collection
 db.instructors.find({});
 
-// b
+// b ) Display all instructors with salaries greater than 4000 (only show firstName and salary)
 db.instructors.find({ salary: { $gt: 4000 } }, { firstName: 1, salary: 1 });
 
-// c
+// c ) Display all instructors with ages less than or equal 25.
 db.instructors.find({ age: { $lte: 25 } });
 
-// d
+// d ) Display all instructors with city mansoura and sreet number 10 or 14 only display (firstname,address,salary).
 db.instructors.find(
   {
     $and: [
@@ -18,60 +18,72 @@ db.instructors.find(
   { firstname: 1, address: 1, salary: 1 }
 );
 
-// e
+// e ) Display all instructors who have js and jquery in their courses (both of them not one of them).
 db.instructors.find({ $and: [{ courses: "js" }, { courses: "jquery" }] });
 
-// f
+// f ) Display number of courses for each instructor and display first name with number of courses.
 db.instructors.find({ courses: { $exists: true } }).forEach((ele) => {
   print(`${ele.firstName} ${ele.courses.length}`);
 });
 
-// g
-db.instructors
+// g ) Write mongodb query to get all instructors that have firstName and lastName properties , sort them by firstName ascending then by
+// lastName descending and finally display their data FullName and age. Note: use mongoDb sort method not javascript array method
+let data = db.instructors
   .find({ firstName: { $exists: true }, lastName: { $exists: true } })
-  .sort({ firstName: 1, lastName: -1 })
-  .forEach((ins) =>
-    print(`FullName: ${ins.firstName} ${ins.lastName}, Age: ${ins.age}`)
-  );
+  .sort({ firstName: 1, lastName: -1 });
 
-// h
+let newData = [];
+let operation = data.forEach((ins) => {
+  let fullName = `${ins.firstName} + " " + ${ins.lastName}`;
+  let age = ins.age;
+  print(`FullName: ${fullName}, Age: ${age}`);
+  newData.push(ins);
+});
+
+// Bonus : create new collection with step g values data
+db.createCollection("instructorsData");
+db.instructorsData.insertMany(newData);
+print(newData);
+
+// h ) Find all instructors that have fullName that contains “mohammed”.
 db.instructors.find({
   $or: [{ firstName: "mohammed" }, { lastName: "mohammed" }],
 });
 
-// i
+// i ) Delete instructor with first name “ebtesam” and has only 5 courses in courses array
 db.instructors.deleteOne({
   $and: [{ firstName: "ebtesam" }, { courses: { $size: 5 } }],
 });
 
-// j
+// j ) Add active property to all instructors and set its value to true.
 db.instructors.updateMany({}, { $set: { active: true } });
 
-// k
+// k ) Change “EF” course to “jquery” for “mazen mohammed” instructor (without knowing EF Index)
 db.instructors.updateOne(
   { firstName: "mazen", lastName: "mohammed", courses: "EF" },
   { $set: { "courses.$": "jquery" } }
 );
 
-// l
+// l ) Add jquery course for “noha hesham”.
 db.instructors.updateOne(
-  { firstName: "noha", lastName: "hesham" },
-  { $addToSet: { courses: "jquery" } }
+  { fullName: /noha hesham/i },
+  { $push: { courses: "jquery" } },
+  { upsert: true }
 );
 
-// m
+// m ) Remove courses property for “ahmed mohammed ” instructor
 db.Instructors.updateOne(
   { firstName: "ahmed", lastName: "mohammed" },
   { $unset: { courses: "" } }
 );
 
-// n
+// n ) Decrease salary by 500 for all instructors that has only 3 courses in their list ($inc)
 db.instructors.updateOne({ courses: { $size: 3 } }, { $inc: { salary: -500 } });
 
-// o
+// o ) Rename address field for all instructors to fullAddress.
 db.instructors.updateMany({}, { $rename: { address: "Full Address" } });
 
-// p
+// p ) Change street number for noha hesham to 20
 db.instructors.updateOne(
   { $and: [{ firstName: "noha" }, { lastName: "hesham" }] },
   { $set: { "Full Address.street": 20 } }
