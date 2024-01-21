a96c860aec26ab417a5e615a4e27ac0658e11b8c
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2024-01-21 12:09:44 +0200

    refactor: :recycle: Update lab3

--- NoSQL/lab3/lab3.js
+++ NoSQL/lab3/lab3.js
@@ -1,226 +1,157 @@
-// 1
+// -------------------- 1 -------------------- //
 
 use FacultySystemV2
 
-db.createCollection("student",{
-    validator:{
-        $jsonSchema:{
-            bsonType:"object",
-            required:["FirstName","LastName","IsFired","FacultyID"],
-            additionalProperties:true,
-            properties:{
-                FirstName:{
-                    bsonType:"string"
-                },
-                LastName:{
-                    bsonType:"string"
-                },
-                IsFired:{
-                    bsonType:"bool"
-                },
-                FacultyID:{
-                    bsonType:"string"
-                }
-            }
-        }
-    }
-})
+db.createCollection("student");
+db.createCollection("faculty");
+db.createCollection("course");
 
-db.createCollection("faculty",{
-    validator:{
-        $jsonSchema:{
-            bsonType:"object",
-            required:["_id","FacultyName","Address"],
-            additionalProperties:false,
-            properties: {
-                _id:{
-                    bsonType:"string"
-                },
-                FacultyName:{
-                    bsonType:"string"
-                },
-                Address:{
-                    bsonType:"string"
-                }
-            }
-        }
-    }
-})
-
-db.createCollection("course",{
-    validator:{
-        $jsonSchema:{
-            bsonType:"object",
-            required:["_id","CourseName","FinalMark"],
-            additionalProperties:true,
-            properties:{
-                _id:{
-                    bsonType:"string"
-                },
-                CourseName:{
-                    bsonType:"string"
-                },
-                FinalMark:{
-                    bsonType:"number"
-                }
-            }
-        }
-    }
-})
-
-db.student.insertMany([
+let studentsInfo = [
   {
-    "FirstName": "Ahmed",
-    "LastName": "Ibrahim",
-    "IsFired": false,
-    "FacultyID": "F001",
-    "Courses": [
-      {
-        "CourseID": "C001",
-        "Grade": 90
-      },
-      {
-        "CourseID": "C002",
-        "Grade": 90
-      },
-      {
-        "CourseID": "C003",
-        "Grade": 90
-      }
-    ]
+    _id: 1,
+    firstName: "Ahmed",
+    lastName: "Alam El-Deen",
+    isFired: false,
+    facultyId: 1,
+    courses: [
+      { courseId: 30, grade: 10 },
+      { courseId: 1, grade: 20 },
+      { courseId: 40, grade: 40 },
+      { courseId: 80, grade: 70 },
+    ],
   },
   {
-    "FirstName": "Mohamed",
-    "LastName": "Ali",
-    "IsFired": true,
-    "FacultyID": "F002",
-    "Courses": [
-      {
-        "CourseID": "C002",
-        "Grade": 95
-      },
-      {
-        "CourseID": "C004",
-        "Grade": 80
-      },
-      {
-        "CourseID": "C005",
-        "Grade": 88
-      }
-    ]
+    _id: 2,
+    firstName: "Mohamed",
+    lastName: "Ali",
+    isFired: false,
+    facultyId: 2,
+    courses: [
+      { courseId: 2, grade: 10 },
+      { courseId: 10, grade: 20 },
+      { courseId: 40, grade: 40 },
+      { courseId: 80, grade: 70 },
+    ],
   },
   {
-    "FirstName": "Ibrahim",
-    "LastName": "Khaled",
-    "IsFired": false,
-    "FacultyID": "F003",
-    "Courses": [
-      {
-        "CourseID": "C001",
-        "Grade": 82
-      },
-      {
-        "CourseID": "C003",
-        "Grade": 96
-      },
-      {
-        "CourseID": "C004",
-        "Grade": 89
-      }
-    ]
-  }
-])
-
-db.faculty.insertMany([
-  {
-    "FacultyName": "Faculty of Social Sciences",
-    "Address": "567 Pine Avenue, Villagetown",
-    "_id": "F004"
+    _id: 3,
+    firstName: "Khaled",
+    lastName: "Ibrahim",
+    isFired: true,
+    facultyId: 3,
+    courses: [
+      { courseId: 3, grade: 10 },
+      { courseId: 10, grade: 20 },
+      { courseId: 40, grade: 40 },
+      { courseId: 80, grade: 70 },
+    ],
   },
   {
-    "FacultyName": "Faculty of Medicine",
-    "Address": "890 Cedar Street, City Springs",
-    "_id": "F005"
+    _id: 4,
+    firstName: "Adam",
+    lastName: "Mahmoud",
+    isFired: false,
+    facultyId: 4,
+    courses: [
+      { courseId: 4, grade: 10 },
+      { courseId: 10, grade: 20 },
+      { courseId: 40, grade: 40 },
+      { courseId: 80, grade: 70 },
+    ],
   },
-  {
-    "FacultyName": "Faculty of Business",
-    "Address": "234 Maple Lane, Townsville",
-    "_id": "F006"
-  }
-])
+];
 
-db.course.insertMany([
-  {
-    "_id": "C006",
-    "CourseName": "Psychology 101",
-    "FinalMark": 88
-  },
-  {
-    "_id": "C007",
-    "CourseName": "Medical Ethics",
-    "FinalMark": 92
-  },
-  {
-    "_id": "C008",
-    "CourseName": "Financial Accounting",
-    "FinalMark": 78
-  },
-  {
-    "_id": "C009",
-    "CourseName": "Introduction to Sociology",
-    "FinalMark": 87
-  },
-  {
-    "_id": "C010",
-    "CourseName": "Software Engineering Principles",
-    "FinalMark": 96
-  }
-])
+let courseInfo = [
+  { _id: 1, courseName: "html", finalMark: 100 },
+  { _id: 2, courseName: "nodeJs", finalMark: 100 },
+  { _id: 3, courseName: "js", finalMark: 200 },
+  { _id: 4, courseName: "react", finalMark: 90 },
+];
 
-////////////////////////////////////////////////////////////////
+let facultyInfo = [
+  { _id: 1, facultyName: "Portsaid", address: "Portsaid" },
+  { _id: 2, facultyName: "AUC", address: "Cairo" },
+  { _id: 3, facultyName: "Canal", address: "Ismaillia" },
+  { _id: 4, facultyName: "Mansoura", address: "Mansoura" },
+];
 
-// 2
+db.student.insertMany(studentsInfo);
+db.course.insertMany(courseInfo);
+db.faculty.insertMany(facultyInfo);
 
-db.student.aggregate([{
- $match:{}
-},{
-    $project:{
-        "fullName":{
-            $concat:["$FirstName"," ","$LastName"]
-        },
-        "average":{
-            $avg:"$Courses.Grade"
-        }
-    }
-}
-])
+db.student.find();
+db.faculty.find();
+db.course.find();
 
-// 3
-
-db.course.aggregate([
-{
-        "$group":{
-            "_id":ObjectId(),
-            "marksSum":{
-                $sum:"$FinalMark"
-        }
-    }
-}
-])
-
-// 4
-
+// -------------------- 2 -------------------- //
 db.student.aggregate([
-{
-    $match:{
-        FirstName:"Ahmed"
-    }
-},{
-    $lookup:{
-        from:"course",
-        localField:"Courses.CourseID",
-        foreignField:"_id",
-        as:"course"
-    }
-}
-])
+  {
+    $unwind: "$courses",
+  }, //
+  {
+    $group: {
+      _id: "$_id",
+      avgGr: { $avg: "$courses.grade" },
+      firstName: { $first: "$firstName" },
+      lastName: { $first: "$lastName" },
+    },
+  },
+  {
+    $project: {
+      avgGrade: {
+        $concat: ["$firstName", " ", "$lastName", { $toString: "$avgGr" }],
+      },
+    },
+  },
+]);
+
+// -------------------- 3 -------------------- //
+db.course.aggregate([
+  {
+    $group: {
+      _id: null,
+      sumFinalMark: { $sum: "$finalMark" },
+    },
+  }, 
+]);
+
+// -------------------- 4 -------------------- //
+db.student.aggregate([
+  {
+    $match: { firstName: "Merna" },
+  },
+
+  {
+    $lookup: {
+      from: "course",
+      localField: "courses.courseId",
+      foreignField: "_id",
+      as: "result",
+    },
+  },
+  {
+    $project: {
+      fullName: { $concat: ["$firstName", " ", "$lastName"] },
+      courseId: "$courses.courseId",
+      result: 1,
+    },
+  },
+]);
+
+// -------------------- 4 -------------------- //
+db.student.aggregate([
+  {
+    $project: {
+      fullName: { $concat: ["$firstName", " ", "$lastName"] },
+      facultyId: 1,
+    },
+  },
+  {
+    $lookup: {
+      from: "faculty",
+      localField: "facultyId",
+      foreignField: "_id",
+      as: "result",
+    },
+  },
+]);
