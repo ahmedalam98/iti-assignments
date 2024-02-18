81f5472a70d67eb9042fd001fe3f79319ff175ce
ahmedalam98 <ahmed.alameldeen.me@gmail.com>
2024-02-18 23:13:06 +0200

    feat: :sparkles: Add lab4

---
+++ Angular/lab4/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'lab4';
+}
---
+++ Angular/lab4/src/app/components/home/home.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './home.component.html',
+  styles: ``,
+})
+export class HomeComponent {
+  myRegForm = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+
+    age: new FormControl(null, [
+      Validators.required,
+      Validators.min(10),
+      Validators.max(30),
+    ]),
+  });
+
+  renderData: Boolean = false;
+
+  get NameValid() {
+    return this.myRegForm.controls['name'].valid;
+  }
+  get AgeValid() {
+    return this.myRegForm.controls['age'].valid;
+  }
+
+  getData() {
+    console.log(this.myRegForm.value);
+
+    this.renderData = true;
+  }
+}
